b9ad327f661d394a71e0a73bb0697400af2473bc
wendy <mizukilee@gmail.com>
2022-11-21 13:50:05 -0500

    commit tree d4524c3320a7dd458ac1c018f08b1dee92282560

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+function writeCards(names,eventName) {
+    const message = []
+    for (let i = 0; i < names.length; i++) { 
+        message.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }  
+
+return message
+}
+
+function countDown(num) {
+  while (num < 0) { 
+    newNum = num--
+    console.log(newNum)
+  }
+}
+
+
+
+
+  
